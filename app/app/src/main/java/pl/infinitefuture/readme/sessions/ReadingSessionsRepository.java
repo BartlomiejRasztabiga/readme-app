68448169943da9db8492ad30076e3a5fea69df86
BartlomiejRasztabiga <bartlomiej.rasztabiga.official@gmail.com>
2017-10-21 17:41:55 +0200

    Refactor package name

---
+++ app/app/src/main/java/pl/infinitefuture/readme/sessions/ReadingSessionsRepository.java
@@ -0,0 +1,70 @@
+package pl.infinitefuture.readme.sessions;
+
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+import pl.infinitefuture.readme.sessions.persistence.ReadingSession;
+import pl.infinitefuture.readme.sessions.persistence.ReadingSessionsDataSource;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class ReadingSessionsRepository implements ReadingSessionsDataSource {
+
+    private static volatile ReadingSessionsRepository instance = null;
+
+    private final ReadingSessionsDataSource mSessionsLocalDataSource;
+
+    private ReadingSessionsRepository(@NonNull ReadingSessionsDataSource sessionsLocalDataSource) {
+        mSessionsLocalDataSource = checkNotNull(sessionsLocalDataSource);
+    }
+
+    public static ReadingSessionsRepository getInstance(ReadingSessionsDataSource sessionsLocalDataSource) {
+        if (instance == null) {
+            synchronized (ReadingSessionsRepository.class) {
+                if (instance == null) {
+                    instance = new ReadingSessionsRepository(sessionsLocalDataSource);
+                }
+            }
+        }
+        return instance;
+    }
+
+    @Override
+    public void getSessions(@NonNull Long bookId, @NonNull LoadSessionsCallback callback) {
+        checkNotNull(callback);
+
+        mSessionsLocalDataSource.getSessions(bookId, new LoadSessionsCallback() {
+            @Override
+            public void onSessionsLoaded(List<ReadingSession> sessions) {
+                callback.onSessionsLoaded(sessions);
+            }
+
+            @Override
+            public void onDataNotAvailable() {
+                callback.onDataNotAvailable();
+            }
+        });
+    }
+
+    @Override
+    public void saveSession(@NonNull ReadingSession session,
+                            @NonNull SaveSessionCallback callback) {
+        mSessionsLocalDataSource.saveSession(session, new SaveSessionCallback() {
+            @Override
+            public void onSessionSaved() {
+                callback.onSessionSaved();
+            }
+
+            @Override
+            public void onDataNotAvailable() {
+                callback.onDataNotAvailable();
+            }
+        });
+    }
+
+    @Override
+    public void saveSession(@NonNull ReadingSession session) {
+        saveSession(session, new SaveSessionCallback() {});
+    }
+}
