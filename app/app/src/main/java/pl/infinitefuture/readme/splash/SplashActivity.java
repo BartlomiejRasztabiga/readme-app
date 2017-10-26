59fcf205d4e612dc7491903570e245600c7c1d06
Bartłomiej Rasztabiga <bartlomiej.rasztabiga.official@gmail.com>
2017-10-26 16:56:48 +0000

    Merged in bugfix/README-67-fix-splash-screen-duration (pull request #26)
    Set minimum splash duration to 0,5 sec

--- app/app/src/main/java/pl/infinitefuture/readme/splash/SplashActivity.java
+++ app/app/src/main/java/pl/infinitefuture/readme/splash/SplashActivity.java
@@ -2,6 +2,7 @@ package pl.infinitefuture.readme.splash;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,12 +10,17 @@ import pl.infinitefuture.readme.books.BooksActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static final int SPLASH_TIME_OUT = 500;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, BooksActivity.class);
-        startActivity(intent);
-        finish();
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, BooksActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_TIME_OUT);
+
     }
 }
