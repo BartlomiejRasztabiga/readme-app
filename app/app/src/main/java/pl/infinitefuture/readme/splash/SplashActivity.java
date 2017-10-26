22d7854ac8cf82cf53b94f842d6f57c33796f9d6
Bartłomiej Rasztabiga <bartlomiej.rasztabiga.official@gmail.com>
2017-10-26 21:14:19 +0200

    Fix splash and bump version to 1.1.3-alpha

--- app/app/src/main/java/pl/infinitefuture/readme/splash/SplashActivity.java
+++ app/app/src/main/java/pl/infinitefuture/readme/splash/SplashActivity.java
@@ -2,7 +2,6 @@ package pl.infinitefuture.readme.splash;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -10,17 +9,12 @@ import pl.infinitefuture.readme.books.BooksActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static final int SPLASH_TIME_OUT = 500;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        new Handler().postDelayed(() -> {
-            Intent intent = new Intent(SplashActivity.this, BooksActivity.class);
-            startActivity(intent);
-            finish();
-        }, SPLASH_TIME_OUT);
-
+        Intent intent = new Intent(SplashActivity.this, BooksActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
