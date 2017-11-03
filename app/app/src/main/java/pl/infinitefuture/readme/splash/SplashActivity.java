c5897e389c62389dd0ae01ca439d23fa7ac6b1a5
Bartłomiej Rasztabiga <bartlomiej.rasztabiga.official@gmail.com>
2017-11-03 21:31:10 +0000

    Merged in feature/README-5-add-notifications (pull request #35)
    Feature/README-5 add notifications
    
    * WIP: Add FCM
    
    * WIP: FCM
    
    * Notifcations: WIP
    
    * Add notifications (basic)

--- app/app/src/main/java/pl/infinitefuture/readme/splash/SplashActivity.java
+++ app/app/src/main/java/pl/infinitefuture/readme/splash/SplashActivity.java
@@ -1,17 +1,31 @@
 package pl.infinitefuture.readme.splash;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.Date;
+
 import pl.infinitefuture.readme.books.BooksActivity;
+import pl.infinitefuture.readme.notifications.AlarmReceiver;
+import pl.infinitefuture.readme.notifications.NotificationScheduler;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //start notification alarm
+        NotificationScheduler.setReminder(SplashActivity.this, AlarmReceiver.class, 17, 0);
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        preferences.edit()
+                .putLong("last_visit", new Date().getTime())
+                .apply();
 
         Intent intent = new Intent(SplashActivity.this, BooksActivity.class);
         startActivity(intent);
